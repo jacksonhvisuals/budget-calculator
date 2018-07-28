e0e7dee2041ad7647d615fc92fa67ee9041a0833
Jackson Hayes <asecretgate@gmail.com>
2018-07-28 12:43:16 -0700

    :recycle: Rename CSS selectors for BudgetInput
    Switch to classes only, and use BEM

--- src/components/BudgetInput/BudgetInput.js
+++ src/components/BudgetInput/BudgetInput.js
@@ -18,7 +18,9 @@ export default class BudgetInput extends Component {
     this.props.calculate();
     let targetvalue = parseFloat(evt.target.value, 10);
     let newWidth = targetvalue.toString().length + "ch";
-    let inputwidth = document.getElementById("budgetInputForm");
+    let inputwidth = document.getElementsByClassName(
+      "HeaderContainer__input"
+    )[0];
     inputwidth.style.width = newWidth;
   }
 
@@ -31,14 +33,17 @@ export default class BudgetInput extends Component {
   returnFunction(event) {
     if (event.keyCode === 13) {
       this.setState({
-        inputValue: document.getElementById("budgetInputForm").value
+        inputValue: document.getElementsByClassName("HeaderContainer__input")[0]
+          .value
       });
       this.props.budgetConfirmHandler(this.state.inputValue);
       this.props.calculate();
     }
   }
   componentDidMount() {
-    let inputwidth = document.getElementById("budgetInputForm");
+    let inputwidth = document.getElementsByClassName(
+      "HeaderContainer__input"
+    )[0];
     let startingWidth = inputwidth.value.length + "ch";
     inputwidth.style.width = startingWidth;
     document.addEventListener("keydown", this.returnFunction, false);
@@ -49,22 +54,18 @@ export default class BudgetInput extends Component {
 
   render() {
     return (
-      <div>
-        <div className="headerContainer">
-          <div className="text-center">
-            <div id="input-icon">$</div>
-            <input
-              type="number"
-              step="0.01"
-              id="budgetInputForm"
-              placeholder="Total amount..."
-              value={this.state.inputValue}
-              onChange={this.updateInputValue}
-              onBlur={this.updateStateValue}
-            />
-          </div>
-          <div id="inputsubtext">TOTAL AMOUNT</div>
-        </div>
+      <div className="HeaderContainer">
+        <div className="HeaderContainer__icon">$</div>
+        <input
+          type="number"
+          step="0.01"
+          className="HeaderContainer__input"
+          placeholder="Total amount..."
+          value={this.state.inputValue}
+          onChange={this.updateInputValue}
+          onBlur={this.updateStateValue}
+        />
+        <div className="HeaderContainer__subtext">TOTAL AMOUNT</div>
       </div>
     );
   }
